f762b5cff55a83ea9c03ee2ee35742f5efafb3f8
ozil-110 <1482666460@qq.com>
2017-06-23 16:51:30 +0800

    racingGame

--- src/js/containers/Main.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.export = require('./App.js');
-} else {
-    module.exports = require('./App.dev.js');
-}
--- src/js/reducers/root.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import postIssues from './index';
-
-const rootReducer = combineReducers({postIssues});
-
-export default rootReducer;
